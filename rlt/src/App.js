6bec2405758a40fcf10c330dc44516e6fe266e36
wkan17012021 <wkan@hotmail.co.uk>
2022-09-18 23:36:45 +0100

    nav comp markup and draft styles remove app css

--- rlt/src/App.js
+++ rlt/src/App.js
@@ -1,14 +1,20 @@
-import './App.css';
+
 import Nav from'./components/navigation/Nav';
 
 function App() {
   return (
-    <body className="App">
+    <div className="App">
       <Nav />
       <header >
-      <p>header element</p>
+      <p>Large hero section with flex img gallery with clickable links or mixkit video background w/ overlay intro welcome text or parallax image</p>
+      <p>Most Popular Tours with mosaic grid responsive layout</p>
+      <p>Small bio text explaining the site</p>
+      <p>full width lazy loading video?</p>
+      <p>Why travel with RLT? animation (gsap) section?</p>
+      <p>Discover section: mentions with JFclub, blog style destination reviews teaser on LHS (links to another page of full blog style reviews or redirects to other travel sites), w/ newsletter signup on RHS</p>
+      <p>Footer: contact links, snapshot gallery, back to top scroll button</p>
       </header>
-    </body>
+    </div>
   );
 }
 
--- rlt/src/components/navigation/Nav.js
+++ rlt/src/components/navigation/Nav.js
@@ -1,17 +1,44 @@
 import React from "react";
+import fossil_emblem from "../../assets/fossil_emblem.jpeg"; 
 
 const Nav = () => {
-    return (
-        <nav>
-            <div class="company_container">
-        <div class="fossil_logo">
-          <img src="" alt="ammonite" />
+  console.log(fossil_emblem);
+  return (
+    <nav>
+      <div className="company_container">
+        <div className="logo_wrapper">
+          <img src={fossil_emblem} alt="ammonite" className="nav_logo" width="50" height="auto"/>
         </div>
-        <div class="company_title">
+        <div className="company_title">
           <h1>The Road Less Travelled</h1>
         </div>
-        </div>
-        </nav>
-    )
-}
+      </div>
+      <ul className="nav_links">
+          <li><a className="home" href="#">Home</a></li>
+          <li><a href="#" target="blank">Destinations</a></li>
+          <li><a href="#" target="blank">About the Site (the build history)</a></li>
+          <li>
+            <a>
+              <i className="fab fa-facebook-square fa-2x"></i>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="fab fa-instagram fa-2x"></i>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="fab fa-twitter-square fa-2x"></i>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="fa fa-bars fa-2x" aria-hidden="true"></i>
+            </a>
+          </li>
+        </ul>
+    </nav>
+  );
+};
 export default Nav;
