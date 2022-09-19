e36c1d6f0632f03805ebc278e59da8e635dc6182
wkan17012021 <wkan@hotmail.co.uk>
2022-09-19 21:36:47 +0100

    draft react tabs comp and style

--- rlt/src/index.js
+++ rlt/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./nav.css";
 import "./flex_panel.css";
+import "./tabs.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
