8a0ed3885b73451dbfb8f861f54a42dff3c42dff
wkan17012021 <wkan@hotmail.co.uk>
2022-10-23 20:50:43 +0100

    scale imgs and place into grid with text

--- rlt/src/components/us_tour/Canyon_tours.js
+++ rlt/src/components/us_tour/Canyon_tours.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Img1 from "../../assets/usa_canyon_photos/antelope_canyon_trail_612x816.jpg";
-import Img2 from "../../assets/usa_canyon_photos/grand_canyon_10721x712.JPG";
+import Img2 from "../../assets/usa_canyon_photos/grand_canyon_754x611.JPG";
 import Img3 from "../../assets/usa_canyon_photos/antelope_canyon_trail_612x816(2).jpg";
 import Img4 from "../../assets/usa_canyon_photos/antelope_canyon_2_500x332.JPG";
-import Img5 from "../../assets/usa_canyon_photos/antelope_canyon_500x500.JPG";
+import Img5 from "../../assets/usa_canyon_photos/antelope_canyon_612x816(3).JPG";
 import Img6 from "../../assets/usa_canyon_photos/grand_canyon_overcast_754x611.jpg";
 
 const CanyonTours = () => {
@@ -30,10 +30,13 @@ const CanyonTours = () => {
           <img alt="text" loading="lazy" src={Img6} />
         </div>
         <div className="txt_wrapper">
-          <p>text</p>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias vitae deleniti eum deserunt omnis, obcaecati aspernatur quo atque dolores iusto, libero suscipit sint esse recusandae accusamus, sapiente fugit dolore itaque ut iste. Minus doloribus harum aliquid illum numquam enim velit, quaerat quod, temporibus voluptates libero possimus amet sint molestiae accusamus suscipit est necessitatibus ipsam, labore culpa. Maxime, dignissimos. Quo molestias sequi provident neque assumenda architecto qui vitae nihil voluptate at.</p>
         </div>
         <div className="txt_wrapper">
-          <p>text</p>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sint enim necessitatibus voluptatibus maxime qui aliquid quam doloribus eaque beatae blanditiis impedit earum reprehenderit nam eligendi aut odit, veritatis nisi suscipit quisquam ex! Perferendis fugiat porro accusantium aliquid? Dicta soluta quia quaerat minus temporibus ullam eaque nesciunt doloremque voluptate. Minus beatae, hic fugiat eius ipsam illum amet eaque totam maxime repudiandae.</p>
+        </div>
+        <div className="txt_wrapper_span">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At corporis cumque voluptates ipsam molestias consequatur accusantium neque totam animi enim delectus accusamus laboriosam aspernatur recusandae libero fugit corrupti error possimus est aliquid minus, culpa voluptate nemo! Repudiandae, corporis eligendi, quo accusamus enim non omnis vel praesentium doloremque laudantium fugit quam.</p>
         </div>
       </div>
     </section>
